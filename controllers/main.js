55d517e89f7bc893f77fd3d7d1eb77aa67d750c5
Chase White <chase.t.white@hotmail.com>
2022-07-04 14:39:47 -0600

    Setting routes and controllers

---
+++ controllers/main.js
@@ -0,0 +1,14 @@
+
+const login = async (req, res) => {
+    res.send('Fake Login/Register/Signup Route');
+};
+
+const dashboard = async (req, res) => {
+    const lucky = Math.floor(Math.random() * 100);
+    res.status(200).json({msg: `Hello, Joe Dirt`, secret: `Here is your authorized data, your lucky number is ${lucky}`});
+};
+
+module.exports = {
+    login,
+    dashboard
+}
